dfddfcdc81889717c6ab15bf82154139b6ddc0d5
Arcusterrr <nikita.kargapolov000@gmail.com>
2020-12-24 18:03:37 +0500

    Добавьте файлы проекта.

---
+++ FitnessApp.BL/Model/Gender.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FitnessApp.BL.Model
+{
+    /// <summary>
+    ///  Пол
+    /// </summary>
+    public class Gender
+    {
+        /// <summary>
+        ///  Название
+        /// </summary>
+        public string Name { get; }
+        /// <summary>
+        /// Создать новый пол
+        /// </summary>
+        /// <param name="_name"> Название пола </param>
+        public Gender(string _name)
+        {
+            if (string.IsNullOrWhiteSpace(_name))
+            {
+                throw new ArgumentNullException("Имя пола не может быть пустым или null", nameof(_name));
+            }
+
+            Name = _name;
+        }
+        /// <summary>
+        /// Переоределение
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
